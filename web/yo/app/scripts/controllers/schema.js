51e52f71277efc01d96696b421e71f030d2ef508
Paul Horn <phorn@avantgarde-labs.de>
2013-08-14 16:42:53 +0200

    show source schema

--- web/yo/app/scripts/controllers/schema.js
+++ web/yo/app/scripts/controllers/schema.js
@@ -1,6 +1,13 @@
 'use strict';
 
 angular.module('dmpApp')
-  .controller('SchemaCtrl', ['$scope', function ($scope) {
+  .controller('SchemaCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.internalName = "Source Target Schema Mapper"
+
+    $scope.sourceSchema = {}
+
+    $http.get('/data/schema.json')
+      .success(function (result) {
+        $scope.sourceSchema = result
+      })
   }])
