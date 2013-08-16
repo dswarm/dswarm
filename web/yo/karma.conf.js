cade8da464e8ce1fc6dc997b874b3c72e8efd4b6
Paul Horn <phorn@avantgarde-labs.de>
2013-08-16 12:32:23 +0200

    update tests for controller

--- web/yo/karma.conf.js
+++ web/yo/karma.conf.js
@@ -9,6 +9,7 @@ files = [
   JASMINE_ADAPTER,
   'app/components/angular/angular.js',
   'app/components/angular-mocks/angular-mocks.js',
+  'app/components/angular-ui-router/release/angular-ui-router.js',
   'app/scripts/*.js',
   'app/scripts/**/*.js',
   'test/mock/**/*.js',
--- web/yo/test/spec/controllers/schema.js
+++ web/yo/test/spec/controllers/schema.js
@@ -4,29 +4,38 @@ beforeEach(module('dmpApp', 'mockedSchema'));
 
 describe('Controller: SchemaCtrl', function () {
 
-  var SchemaCtrl,
+  var schemaCtrl,
     scope,
-    mockedSchema;
+    $httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $httpBackend, $rootScope, mockSchemaJSON) {
+  beforeEach(inject(function ($controller, _$httpBackend_, $rootScope, mockSchemaJSON) {
       scope = $rootScope.$new();
-
+      $httpBackend = _$httpBackend_;
       $httpBackend.whenGET('/data/schema.json').respond(mockSchemaJSON);
 
-      SchemaCtrl = $controller('SchemaCtrl', {
-        $scope: scope
-      });
+      schemaCtrl = function() {
+        return $controller('SchemaCtrl', {
+          '$scope': scope
+        });
+      };
+    }
+  ));
 
-      $httpBackend.flush();
-
+  afterEach(inject(function () {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
   }));
 
-  it('should have loaded schema data', function () {
+  it('should have loaded schema data', inject(function () {
+      $httpBackend.expectGET('/data/schema.json');
+      schemaCtrl();
+      $httpBackend.flush();
 
       expect(scope.data.name).toBe('OAI-PMH');
 
       expect(scope.data.children.length).toBe(3);
 
-  });
+    }
+  ));
 });
