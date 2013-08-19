31744cd0550a555f9c5cb2fb49cc9b2dab140870
Paul Horn <phorn@avantgarde-labs.de>
2013-08-19 15:27:20 +0200

    partially implement draggable component
    WIP, there's something left to o

--- web/yo/app/scripts/controllers/components.js
+++ web/yo/app/scripts/controllers/components.js
@@ -10,6 +10,30 @@ angular.module('dmpApp')
       'children': []
     }
 
+    $scope.onLeafClick = function (event, data) {
+      var $el = angular.element(event.target)
+
+      if (!$el.data('uiDraggable')) {
+        $el.draggable({
+          cursor: 'move',
+          cursorAt: {top: -5, left: -5},
+          opacity: 0.7,
+          containment: '#transformation',
+          revert: 'invalid',
+          appendTo: 'body',
+//          helper: 'clone',
+          helper: function() {
+            var $el = angular.element('<div class="component">' + data.name + '</div>')
+            $el.data('component-type', 'function')
+              .data('payload', data);
+            return $el
+          }
+        })
+      }
+      console.log('on leaf click');
+      console.log(data);
+    }
+
     $http.get('/data/functions.json')
       .success(function (result) {
         $scope.functions.children = result['functions']
