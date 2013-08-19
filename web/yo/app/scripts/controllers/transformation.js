fc953428b664c0bff8f72dde21309aa403df5c99
Paul Horn <phorn@avantgarde-labs.de>
2013-08-19 16:40:12 +0200

    add rudimental drop-able functionality

--- web/yo/app/scripts/controllers/transformation.js
+++ web/yo/app/scripts/controllers/transformation.js
@@ -2,5 +2,20 @@
 
 angular.module('dmpApp')
   .controller('TransformationCtrl', ['$scope', function ($scope) {
-    $scope.internalName = 'Transformation Logic Widget'
-  }])
+    $scope.internalName = 'Transformation Logic Widget';
+
+    $scope.components = [];
+
+
+    $scope.droppableOptions = {
+      accept: '.tree-leaf',
+      activeClass: 'ui-state-hover',
+      hoverClass: 'ui-state-active',
+      drop: function(event, ui) {
+        $scope.components.push(ui.helper.data());
+        $scope.$digest();
+      }
+
+    };
+
+  }]);
