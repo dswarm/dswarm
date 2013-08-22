5353caaea0c4c3b111f0168e89eec1f20fede81e
Paul Horn <phorn@avantgarde-labs.de>
2013-08-22 20:07:16 +0200

    update to bootstrap 3 final

--- web/yo/karma.conf.js
+++ web/yo/karma.conf.js
@@ -1,58 +1,80 @@
-// Karma configuration
+module.exports = function(config) {
+    config.set({
+        // Karma configuration
 
-// base path, that will be used to resolve files and exclude
-basePath = 'app/';
+        // base path, that will be used to resolve files and exclude
+        basePath : 'app/',
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'components/angular/angular.js',
-  'components/angular-mocks/angular-mocks.js',
-  'components/angular-ui-router/release/angular-ui-router.js',
-  'scripts/*.js',
-  'scripts/**/*.js',
-  'views/directives/**/*.html',
-  '../test/mock/**/*.js',
-  '../test/spec/**/*.js'
-];
+        // list of files / patterns to load in the browser
+        files : [
+          'components/angular/angular.js',
+          'components/angular-mocks/angular-mocks.js',
+          'components/angular-ui-bootstrap/misc/test-lib/angular-mocks.js',
+          'components/angular-ui-utils/components/angular-ui-docs/build/ui-utils.js',
+          'components/angular-resource/angular-resource.js',
+          'components/angular-cookies/angular-cookies.js',
+          'components/angular-sanitize/angular-sanitize.js',
+          'components/angular-ui-router/release/angular-ui-router.js',
+          'scripts/*.js',
+          'scripts/**/*.js',
 
-// list of files to exclude
-exclude = [];
+          // templates
+          'views/directives/**/*.html',
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+          '../test/mock/**/*.js',
+          '../test/spec/**/*.js'
+        ],
 
-// web server port
-port = 8080;
+        // list of files to exclude
+        exclude : [],
 
-// cli runner port
-runnerPort = 9100;
+        // test results reporter to use
+        // possible values: dots || progress || growl
+        //reporters : ['progress'],
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+        // web server port
+        //port : 8080,
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+        // cli runner port
+        //runnerPort : 9100,
+        
+        // enable / disable colors in the output (reporters and logs)
+        colors : true,
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+        // level of logging
+        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        //logLevel : LOG_DEBUG,
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch : true,
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers : ['Chrome'],
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout : 5000,
+
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun : false,
+
+        junitReporter : {
+            outputFile: 'test_out/unit.xml',
+            suite: 'unit'
+        },
+
+        frameworks : ["jasmine"],
+
+        preprocessors : {
+            '**/*.html': ['ng-html2js']
+        }
+
+    })
+};
