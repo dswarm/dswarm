030a46e6332e5d27b4ebf6ffc2a59ef61938c7e7
Thomas Gängler <tgaengler@avantgarde-labs.de>
2013-08-28 16:56:49 +0200

    setup jersey with dummy resource and test object

---
+++ controller/src/main/java/de/avgl/dmp/controller/DMPControllerException.java
@@ -0,0 +1,29 @@
+package de.avgl.dmp.controller;
+
+/**
+ * The exception class for DMP controller exceptions.<br>
+ * 
+ * @author tgaengler (created), May 22, 2013
+ * @author $Author$ (last changed)
+ * @version $Rev$, $Date$<br>
+ *          $Id: $
+ */
+
+public class DMPControllerException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Creates a new DMP controller exception with the given exception message.
+	 * 
+	 * @param exception the exception message
+	 */
+	public DMPControllerException(final String exception) {
+
+		super(exception);
+	}
+
+}
---
+++ controller/src/test/java/de/avgl/dmp/controller/resources/test/MyResourceTest.java
@@ -0,0 +1,67 @@
+package de.avgl.dmp.controller.resources.test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.avgl.dmp.controller.Main;
+
+public class MyResourceTest {
+
+	private HttpServer server;
+	private WebTarget target;
+
+	@Before
+	public void setUp() throws Exception {
+		// start the server
+		server = Main.startServer();
+		// create the client
+		Client c = ClientBuilder.newClient();
+
+		target = c.target(Main.BASE_URI);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		server.stop();
+	}
+
+	/**
+	 * Test to see that the message "Got it!" is sent in the response.
+	 */
+	@Test
+	public void testGetItJSON() {
+		String responseMsg = target.path("myresource").request()
+				.accept(MediaType.APPLICATION_JSON).get(String.class);
+		Assert.assertEquals("{\"message\":\"Hello World\"}", responseMsg);
+	}
+
+	/**
+	 * Test to see that the message "Got it!" is sent in the response.
+	 */
+	@Test
+	public void testGetItXML() {
+		String responseMsg = target.path("myresource").request()
+				.accept(MediaType.APPLICATION_XML).get(String.class);
+		Assert.assertEquals(
+				"<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><testObject><message>Hello World</message></testObject>",
+				responseMsg);
+	}
+
+	/**
+	 * Test to see that the message "Got it!" is sent in the response.
+	 */
+	@Test
+	public void testGetIt() {
+		String responseMsg = target.path("myresource").request()
+				.accept(MediaType.TEXT_PLAIN).get(String.class);
+		Assert.assertEquals("Got it!", responseMsg);
+	}
+}
