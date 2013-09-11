64f64c744221e08d0e512aa545c1ea1ac8eb2ed2
Thomas Gängler <tgaengler@avantgarde-labs.de>
2013-09-11 18:44:19 +0200

    [CONVERTER] added test CSV file and dummy test for CSV file

--- converter/src/test/java/de/avgl/dmp/converter/flow/test/TransformationFlowTest.java
+++ converter/src/test/java/de/avgl/dmp/converter/flow/test/TransformationFlowTest.java
@@ -2,8 +2,13 @@ package de.avgl.dmp.converter.flow.test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.StringWriter;
 import java.util.List;
 
+import org.culturegraph.mf.stream.converter.JsonEncoder;
+import org.culturegraph.mf.stream.reader.CsvReader;
+import org.culturegraph.mf.stream.sink.ObjectJavaIoWriter;
+import org.culturegraph.mf.stream.source.StringReader;
 import org.junit.Test;
 
 import de.avgl.dmp.converter.flow.TransformationFlow;
@@ -11,10 +16,8 @@ import de.avgl.dmp.init.util.DMPUtil;
 import de.avgl.dmp.persistence.mapping.JsonToPojoMapper;
 import de.avgl.dmp.persistence.model.Transformation;
 
-
-
 public class TransformationFlowTest {
-	
+
 	@Test
 	public void testEndToEnd() throws Exception {
 
@@ -40,7 +43,7 @@ public class TransformationFlowTest {
 
 		assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void testEndToEndByRecordStringExample() throws Exception {
 
@@ -52,5 +55,24 @@ public class TransformationFlowTest {
 		final String actual = flow.applyRecord(request);
 
 		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void readCSVTest() throws Exception {
+
+		StringReader opener = new StringReader();
+		final String testCSVString = DMPUtil.getResourceAsString("test_csv.csv");
+
+		final CsvReader reader = new CsvReader();
+		reader.setHasHeader(true);
+		final JsonEncoder converter = new JsonEncoder();
+		final StringWriter stringWriter = new StringWriter();
+		final ObjectJavaIoWriter<String> writer = new ObjectJavaIoWriter<>(stringWriter);
+
+		opener.setReceiver(reader).setReceiver(converter).setReceiver(writer);
+
+		opener.process(testCSVString);
+
+		System.out.println(stringWriter.toString());
 	}
 }
