3cd1700a3c36006dca0f0d3caf68c5ac80555144
Thomas Gängler <tgaengler@avantgarde-labs.de>
2013-09-12 18:41:10 +0200

    [PERSISTENCE] extended service tests

--- persistence/src/test/java/de/avgl/dmp/persistence/services/test/ConfigurationServiceTest.java
+++ persistence/src/test/java/de/avgl/dmp/persistence/services/test/ConfigurationServiceTest.java
@@ -1,5 +1,11 @@
 package de.avgl.dmp.persistence.services.test;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import de.avgl.dmp.init.util.DMPUtil;
 import de.avgl.dmp.persistence.model.resource.Configuration;
 import de.avgl.dmp.persistence.model.test.BasicJPAServiceTest;
 import de.avgl.dmp.persistence.services.ConfigurationService;
@@ -12,5 +18,31 @@ public class ConfigurationServiceTest extends BasicJPAServiceTest<Configuration,
 	public ConfigurationServiceTest() {
 		
 		super("configuration", ConfigurationService.class);
+	}
+	
+	@Test
+	public void testSimpleConfiguration() {
+		
+		Configuration configuration = createObject();
+		
+		
+		final ObjectNode parameters = new ObjectNode(DMPUtil.getJSONFactory());
+		final String parameterKey = "fileseparator";
+		final String parameterValue = ";";
+		parameters.put(parameterKey, parameterValue);
+		
+		configuration.setParameters(parameters);
+		
+		updateObject(configuration);
+		
+		Configuration updatedConfiguration = getUpdatedObject(configuration);
+		
+		Assert.assertNotNull("the configuration of the updated resource shouldn't be null", updatedConfiguration.getParameters());
+		Assert.assertEquals("the configurations of the resource are not equal", configuration.getParameters(), updatedConfiguration.getParameters());
+		Assert.assertNotNull("the parameter value shouldn't be null", configuration.getParameter(parameterKey));
+		Assert.assertEquals("the parameter value should be equal", configuration.getParameter(parameterKey).asText(), parameterValue);
+		
+		// clean up DB
+		deletedObject(configuration.getId());
 	}
 }
--- persistence/src/test/java/de/avgl/dmp/persistence/services/test/ResourceServiceTest.java
+++ persistence/src/test/java/de/avgl/dmp/persistence/services/test/ResourceServiceTest.java
@@ -11,35 +11,35 @@ import de.avgl.dmp.persistence.model.resource.ResourceType;
 import de.avgl.dmp.persistence.model.test.BasicJPAServiceTest;
 import de.avgl.dmp.persistence.services.ResourceService;
 
-
 public class ResourceServiceTest extends BasicJPAServiceTest<Resource, ResourceService> {
-	
-	private static final org.apache.log4j.Logger	LOG					= org.apache.log4j.Logger.getLogger(ResourceServiceTest.class);
-	
+
+	private static final org.apache.log4j.Logger	LOG	= org.apache.log4j.Logger.getLogger(ResourceServiceTest.class);
+
 	public ResourceServiceTest() {
-		
+
 		super("resource", ResourceService.class);
 	}
-	
+
 	@Test
 	public void testSimpleResource() {
-		
-		
+
 		Resource resource = createObject();
-		
+
 		resource.setName("bla");
 		resource.setDescription("blubblub");
 		resource.setType(ResourceType.FILE);
-		
+
 		final ObjectNode attributes = new ObjectNode(DMPUtil.getJSONFactory());
-		attributes.put("path", "/path/to/file.end");
-		
+		final String attributeKey = "path";
+		final String attributeValue = "/path/to/file.end";
+		attributes.put(attributeKey, attributeValue);
+
 		resource.setAttributes(attributes);
-		
+
 		updateObject(resource);
-		
+
 		Resource updatedResource = getUpdatedObject(resource);
-		
+
 		Assert.assertNotNull("the name of the updated resource shouldn't be null", updatedResource.getName());
 		Assert.assertEquals("the names of the resource are not equal", resource.getName(), updatedResource.getName());
 		Assert.assertNotNull("the description of the updated resource shouldn't be null", updatedResource.getDescription());
@@ -48,7 +48,9 @@ public class ResourceServiceTest extends BasicJPAServiceTest<Resource, ResourceS
 		Assert.assertEquals("the types of the resource are not equal", resource.getType(), updatedResource.getType());
 		Assert.assertNotNull("the attribute of the updated resource shouldn't be null", updatedResource.getAttributes());
 		Assert.assertEquals("the attributes of the resource are not equal", resource.getAttributes(), updatedResource.getAttributes());
-		
+		Assert.assertNotNull("the attribute value shouldn't be null", resource.getAttribute(attributeKey));
+		Assert.assertEquals("the attribute value should be equal", resource.getAttribute(attributeKey).asText(), attributeValue);
+
 		// clean up DB
 		deletedObject(resource.getId());
 	}
