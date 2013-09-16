906309a1ec15d834968f2a529f6c00f9c2992979
Thomas Gängler <tgaengler@avantgarde-labs.de>
2013-09-16 11:35:23 +0200

    [CONVERTER] tried to fix cobertura/java7 problems

--- converter/src/main/java/de/avgl/dmp/converter/morph/MorphScriptBuilder.java
+++ converter/src/main/java/de/avgl/dmp/converter/morph/MorphScriptBuilder.java
@@ -5,6 +5,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -22,33 +24,77 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
 
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
+import com.google.common.io.Resources;
 
 import de.avgl.dmp.converter.DMPConverterException;
 import de.avgl.dmp.persistence.model.transformation.Component;
 import de.avgl.dmp.persistence.model.transformation.Parameter;
 import de.avgl.dmp.persistence.model.transformation.Transformation;
 
-
 public class MorphScriptBuilder {
 
-	private static final DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+	private static final org.apache.log4j.Logger	LOG					= org.apache.log4j.Logger.getLogger(MorphScriptBuilder.class);
+	
+	private static final DocumentBuilderFactory	docFactory					= DocumentBuilderFactory.newInstance();
 
-	private static final TransformerFactory transformerFactory;
+	private static final String					SCHEMA_PATH					= "schemata/metamorph.xsd";
+
+	private static final TransformerFactory		transformerFactory;
+
+	private static final String					TRANSFORMER_FACTORY_CLASS	= "com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl";
 
 	static {
+		System.setProperty("javax.xml.transform.TransformerFactory", TRANSFORMER_FACTORY_CLASS);
 		transformerFactory = TransformerFactory.newInstance();
 		transformerFactory.setAttribute("indent-number", 4);
+
+		InputStream SCHEMA_STREAM = null;
+		
+		try {
+			
+			SCHEMA_STREAM = Resources.newInputStreamSupplier(Resources.getResource(SCHEMA_PATH)).getInput();
+		} catch (final IOException e1) {
+			
+			e1.printStackTrace();
+		}
+		
+		if(SCHEMA_STREAM == null) {
+			
+			LOG.error("couldn't read schema resource");
+		}
+		
+		// final StreamSource SCHEMA_SOURCE = new StreamSource(SCHEMA_STREAM);
+		final SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+		Schema schema = null;
+
+		try {
+
+			// TODO: dummy schema right now, since it couldn't parse the metamorph schema for some reason
+			schema = sf.newSchema();
+		} catch (final SAXException e) {
+
+			e.printStackTrace();
+		}
+		
+		if(schema == null) {
+			
+			LOG.error("couldn't parse schema");
+		}
+
+		docFactory.setSchema(schema);
 	}
 
-	private Document doc;
-
+	private Document							doc;
 
 	private void createParameters(final Map<String, Parameter> parameters, Element component) {
 		if (parameters != null) {
@@ -93,7 +139,7 @@ public class MorphScriptBuilder {
 			return null;
 		}
 
-		transformer.setOutputProperty(OutputKeys.INDENT, indent? "yes" : "no");
+		transformer.setOutputProperty(OutputKeys.INDENT, indent ? "yes" : "no");
 
 		transformer.setOutputProperty(OutputKeys.ENCODING, defaultEncoding);
 
@@ -156,8 +202,10 @@ public class MorphScriptBuilder {
 
 		Element rootElement = doc.createElement("metamorph");
 		rootElement.setAttribute("xmlns", "http://www.culturegraph.org/metamorph");
-		rootElement.setAttribute("version", "1");
+		rootElement.setAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+		rootElement.setAttribute("xsi:schemaLocation", "http://www.culturegraph.org/metamorph metamorph.xsd");
 		rootElement.setAttribute("entityMarker", ".");
+		rootElement.setAttribute("version", "1");
 		doc.appendChild(rootElement);
 
 		Element meta = doc.createElement("meta");
