8ab84a5c5eff511eae44a5f3baed796da1202f0b
Thomas Gängler <tgaengler@avantgarde-labs.de>
2013-11-15 17:13:14 +0100

    [PERSISTENCE] added MappingGroup domain model POJO

---
+++ persistence/src/main/java/de/avgl/dmp/persistence/model/job/MappingGroup.java
@@ -0,0 +1,46 @@
+package de.avgl.dmp.persistence.model.job;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author tgaengler
+ */
+@XmlRootElement
+public class MappingGroup extends BasicDMPObject {
+
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= 1L;
+
+	private List<Mapping>		mappings;
+
+	private String				description			= null;
+
+	public MappingGroup(final String id) {
+
+		super(id);
+	}
+
+	public List<Mapping> getMappings() {
+
+		return mappings;
+	}
+
+	public void setMappings(final List<Mapping> mappingsArg) {
+
+		mappings = mappingsArg;
+	}
+
+	public String getDescription() {
+
+		return description;
+	}
+
+	public void setDescription(final String descriptionArg) {
+
+		description = descriptionArg;
+	}
+}
--- persistence/src/main/java/de/avgl/dmp/persistence/model/job/Task.java
+++ persistence/src/main/java/de/avgl/dmp/persistence/model/job/Task.java
@@ -1,6 +1,58 @@
 package de.avgl.dmp.persistence.model.job;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
-public class Task {
+/**
+ * @author tgaengler
+ */
+@XmlRootElement
+public class Task extends BasicDMPObject {
 
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= 1L;
+
+	@XmlElement(name = "input_data_model")
+	private DataModel			inputDataModel		= null;
+
+	@XmlElement(name = "output_data_model")
+	private DataModel			outputDataModel		= null;
+
+	private Job					job					= null;
+
+	public Task(final String id) {
+
+		super(id);
+	}
+
+	public DataModel getInputDataModel() {
+		return inputDataModel;
+	}
+
+	public void setInputDataModel(final DataModel inputDataModelArg) {
+
+		inputDataModel = inputDataModelArg;
+	}
+
+	public DataModel getOutputDataModel() {
+
+		return outputDataModel;
+	}
+
+	public void setOutputDataModel(final DataModel outputDataModelArg) {
+
+		outputDataModel = outputDataModelArg;
+	}
+
+	public Job getJob() {
+
+		return job;
+	}
+
+	public void setJob(final Job jobArg) {
+
+		job = jobArg;
+	}
 }
