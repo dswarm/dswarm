3810e8db79e299478a40a95e85d43f9d3c1811f3
Thomas Gängler <tgaengler@avantgarde-labs.de>
2013-12-05 15:19:01 +0100

    [PERSISTENCE] added Project POJO

---
+++ persistence/src/main/java/de/avgl/dmp/persistence/model/utils/SetFunctionReferenceSerializer.java
@@ -0,0 +1,7 @@
+package de.avgl.dmp.persistence.model.utils;
+
+import de.avgl.dmp.persistence.model.job.Function;
+
+public class SetFunctionReferenceSerializer extends SetReferenceSerializer<Function> {
+
+}
