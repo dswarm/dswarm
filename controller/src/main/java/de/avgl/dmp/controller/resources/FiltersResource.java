7dfaa9b1a92c8f4abcef675cfc7e4ca1bc62878d
Thomas Gängler <tgaengler@avantgarde-labs.de>
2013-12-11 16:08:00 +0100

    [CTRL] finished Filter controller service (incl. test)

--- controller/src/main/java/de/avgl/dmp/controller/resources/FiltersResource.java
+++ controller/src/main/java/de/avgl/dmp/controller/resources/FiltersResource.java
@@ -1,8 +1,14 @@
 package de.avgl.dmp.controller.resources;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
@@ -22,7 +28,7 @@ import de.avgl.dmp.persistence.service.job.FilterService;
 @RequestScoped
 @Api(value = "/filters", description = "Operations about filters.")
 @Path("filters")
-public class FiltersResource extends BasicResource<FilterService, Filter, Long> {
+public class FiltersResource extends BasicDMPResource<FilterService, Filter> {
 
 	@Context
 	UriInfo											uri;
@@ -34,13 +40,19 @@ public class FiltersResource extends BasicResource<FilterService, Filter, Long> 
 	}
 
 	@ApiOperation(value = "get the filter that matches the given id", notes = "Returns the Filter object that matches the given id.")
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
 	@Override
-	public Response getObject(@ApiParam(value = "filter identifier", required = true) final Long id) throws DMPControllerException {
+	public Response getObject(@ApiParam(value = "filter identifier", required = true) @PathParam("id") final Long id) throws DMPControllerException {
 
 		return super.getObject(id);
 	}
 
 	@ApiOperation(value = "create a new filter", notes = "Returns a new Filter object.", response = Filter.class)
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	@Override
 	public Response createObject(@ApiParam(value = "filter (as JSON)", required = true) final String jsonObjectString)
 			throws DMPControllerException {
@@ -49,6 +61,8 @@ public class FiltersResource extends BasicResource<FilterService, Filter, Long> 
 	}
 
 	@ApiOperation(value = "get all filters ", notes = "Returns a list of Filter objects.")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
 	@Override
 	public Response getObjects() throws DMPControllerException {
 
@@ -58,7 +72,8 @@ public class FiltersResource extends BasicResource<FilterService, Filter, Long> 
 	@Override
 	protected Filter prepareObjectForUpdate(final Filter objectFromJSON, final Filter object) {
 
-		object.setName(objectFromJSON.getName());
+		super.prepareObjectForUpdate(objectFromJSON, object);
+		
 		object.setExpression(objectFromJSON.getExpression());
 
 		return object;
