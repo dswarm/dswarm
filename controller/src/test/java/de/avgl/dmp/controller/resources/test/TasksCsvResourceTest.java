6ffe6ea2d27566afde8af3c0544f5c6c86dba009
Thomas Gängler <tgaengler@avantgarde-labs.de>
2014-01-21 09:16:30 +0100

    [CONTROLLER] fixed last missing test

--- controller/src/test/java/de/avgl/dmp/controller/resources/test/TasksCsvResourceTest.java
+++ controller/src/test/java/de/avgl/dmp/controller/resources/test/TasksCsvResourceTest.java
@@ -169,7 +169,7 @@ public class TasksCsvResourceTest extends ResourceTest {
 		final ObjectNode outputAttributePathAttributeJSON = (ObjectNode) ((ArrayNode) ((ObjectNode) mappingJSON.get("output_attribute_path"))
 				.get("attributes")).get(0);
 		final String outputAttributeName = outputAttributePathAttributeJSON.get("name").asText();
-		outputAttributePathAttributeJSON.put("id", dataResourceSchemaBaseURI + outputAttributeName);
+		outputAttributePathAttributeJSON.put("uri", dataResourceSchemaBaseURI + outputAttributeName);
 
 		final ArrayNode inputAttributePathsJSON = (ArrayNode) mappingJSON.get("input_attribute_paths");
 
@@ -177,7 +177,7 @@ public class TasksCsvResourceTest extends ResourceTest {
 
 			final ObjectNode inputAttributeJSON = (ObjectNode) ((ArrayNode) ((ObjectNode) inputAttributePathsJSONNode).get("attributes")).get(0);
 			final String inputAttributeName = inputAttributeJSON.get("name").asText();
-			inputAttributeJSON.put("id", dataResourceSchemaBaseURI + inputAttributeName);
+			inputAttributeJSON.put("uri", dataResourceSchemaBaseURI + inputAttributeName);
 		}
 
 		final String finalTaskJSONString = objectMapper.writeValueAsString(taskJSON);
