32f264524b060d428e51e6cd9316ad63a8622b47
Paul Horn <phorn@avantgarde-labs.de>
2014-04-04 11:26:33 +0200

    [TASK] Add bash script to reset all databases

---
+++ dev-tools/reset-dbs.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DIR="$( dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ))"
+
+MYSQL_PW=${MYSQL_PW:="dmp_mysql"}
+MYSQL_UN=${MYSQL_UN:="dmp"}
+MYSQL_DB=${MYSQL_DB:="dmp_dev"}
+
+NEO4J_URL=${NEO4J_URL:="http://localhost:7474/graph"}
+
+cd ${DIR}
+
+mysql -f -u${MYSQL_UN} -p${MYSQL_PW} ${MYSQL_DB} < persistence/src/main/resources/schema.sql
+mysql -f -u${MYSQL_UN} -p${MYSQL_PW} ${MYSQL_DB} < persistence/src/main/resources/functions.sql
+
+curl -v -X DELETE "${NEO4J_URL}/maintain/delete"
