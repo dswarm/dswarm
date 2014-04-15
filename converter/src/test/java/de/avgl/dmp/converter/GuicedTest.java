3f06824dcd32bf2d7862649aa5357327783b5e1b
Paul Horn <phorn@avantgarde-labs.de>
2014-04-15 11:06:07 +0200

    [FIX][DD-413] add ObjectMapper creation to all GuicedTests
    - previously, this was part of the PersistenceModule, where it is now removed from
    - It is required for the tests to use the Jaxb Annotations

--- converter/src/test/java/de/avgl/dmp/converter/GuicedTest.java
+++ converter/src/test/java/de/avgl/dmp/converter/GuicedTest.java
@@ -1,8 +1,14 @@
 package de.avgl.dmp.converter;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
 import com.google.common.eventbus.EventBus;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
 import com.google.inject.persist.PersistService;
 import com.google.inject.persist.jpa.JpaPersistModule;
 import org.junit.AfterClass;
@@ -20,6 +26,26 @@ public class GuicedTest {
 			@Override
 			protected EventBus provideEventBus() {
 				return new EventBus();
+			}
+
+			/**
+			 * Provides the {@link com.fasterxml.jackson.databind.ObjectMapper} instance for JSON de-/serialisation.
+			 * @return a {@link com.fasterxml.jackson.databind.ObjectMapper} instance as singleton
+			 */
+			@Provides
+			@Singleton
+			protected ObjectMapper provideObjectMapper() {
+
+				final ObjectMapper mapper = new ObjectMapper();
+				final JaxbAnnotationModule module = new JaxbAnnotationModule();
+
+				mapper
+						.registerModule(module)
+						.registerModule(new Hibernate4Module())
+						.setSerializationInclusion(JsonInclude.Include.NON_NULL)
+						.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+
+				return mapper;
 			}
 		}
 
