7474ad4d11aaa539338ee3c2f332fd54ab8f5221
Thomas Gängler <tgaengler@avantgarde-labs.de>
2014-04-22 16:31:45 +0200

    [FEATURE][DD-398][DD-404][CONVERTER] added test for filter with multiple results

--- converter/src/test/java/de/avgl/dmp/converter/flow/test/FilterTransformationFlowTest.java
+++ converter/src/test/java/de/avgl/dmp/converter/flow/test/FilterTransformationFlowTest.java
@@ -2,9 +2,13 @@ package de.avgl.dmp.converter.flow.test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
+
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -28,13 +32,39 @@ public class FilterTransformationFlowTest extends GuicedTest {
 
 		final String actual = flow.applyResource("test-mabxml.tuples.json");
 
+		final ArrayNode expectedJson = replaceKeyWithActualKey(expected, actual);
+		final String finalExpected = DMPPersistenceUtil.getJSONObjectMapper().writeValueAsString(expectedJson);
+
+		assertEquals(finalExpected, actual);
+	}
+
+	@Test
+	public void testFilterEndToEndWithMultipleResults() throws Exception {
+
+		final String expected = DMPPersistenceUtil.getResourceAsString("test-mabxml.filter.result.2.json");
+
+		final Provider<InternalModelServiceFactory> internalModelServiceFactoryProvider = injector.getProvider(InternalModelServiceFactory.class);
+
+		final TransformationFlow flow = TransformationFlow.fromFile("filtermorph2.xml", internalModelServiceFactoryProvider);
+
+		final String actual = flow.applyResource("test-mabxml.tuples.2.json");
+
+		final ArrayNode expectedJson = replaceKeyWithActualKey(expected, actual);
+		final String finalExpected = DMPPersistenceUtil.getJSONObjectMapper().writeValueAsString(expectedJson);
+
+		assertEquals(finalExpected, actual);
+	}
+
+	private ArrayNode replaceKeyWithActualKey(final String expected, final String actual) throws JsonParseException, JsonMappingException,
+			IOException {
+
 		// replace key with actual key
 		final ArrayNode expectedJson = DMPPersistenceUtil.getJSONObjectMapper().readValue(expected, ArrayNode.class);
 		final ObjectNode expectedTuple = (ObjectNode) expectedJson.get(0);
 		final String expectedFieldName = expectedTuple.fieldNames().next();
 		final ArrayNode expectedContent = (ArrayNode) expectedTuple.get(expectedFieldName);
 		final ObjectNode expectedContentJson = (ObjectNode) expectedContent.get(0);
-		final String expectedContentValue = expectedContentJson.get(expectedContentJson.fieldNames().next()).asText();
+		final JsonNode expectedContentValue = expectedContentJson.get(expectedContentJson.fieldNames().next());
 
 		Assert.assertNotNull("the actual transformation result shouldn't be null", actual);
 		final ArrayNode actualJson = DMPPersistenceUtil.getJSONObjectMapper().readValue(actual, ArrayNode.class);
@@ -55,8 +85,6 @@ public class FilterTransformationFlowTest extends GuicedTest {
 		newExpectedContent.add(newExpectedContentJson);
 		expectedTuple.put(expectedFieldName, newExpectedContent);
 
-		final String finalExpected = DMPPersistenceUtil.getJSONObjectMapper().writeValueAsString(expectedJson);
-
-		assertEquals(finalExpected, actual);
+		return expectedJson;
 	}
 }
