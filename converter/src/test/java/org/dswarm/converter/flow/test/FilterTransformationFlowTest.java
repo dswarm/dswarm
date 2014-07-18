d2cad0366295a76e0eb7405326e5b06eb661e364
Thomas Gängler <tgaengler@avantgarde-labs.de>
2014-07-18 14:44:26 +0200

    [FEATURE][DD-624] provided test for filter in collector with hiarchical entity

--- converter/src/test/java/org/dswarm/converter/flow/test/FilterTransformationFlowTest.java
+++ converter/src/test/java/org/dswarm/converter/flow/test/FilterTransformationFlowTest.java
@@ -1,6 +1,7 @@
 package org.dswarm.converter.flow.test;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.dswarm.converter.GuicedTest;
 import org.dswarm.converter.flow.TransformationFlow;
@@ -99,6 +100,24 @@ public class FilterTransformationFlowTest extends GuicedTest {
 	}
 
 	@Test
+	public void testFilterAndSelectingValueIsOnAnotherHierarchy() throws Exception {
+
+		final String expected = DMPPersistenceUtil.getResourceAsString("test-ralfs_mabxml.filter.result.5.json");
+
+		final Provider<InternalModelServiceFactory> internalModelServiceFactoryProvider = GuicedTest.injector
+				.getProvider(InternalModelServiceFactory.class);
+
+		final TransformationFlow flow = TransformationFlow.fromFile("filtermorph5.xml", internalModelServiceFactoryProvider);
+
+		final String actual = flow.applyResource("ralfs_mabxml.tuples.json");
+
+		final ArrayNode expectedJson = replaceKeyWithActualKey(expected, actual);
+		final String finalExpected = DMPPersistenceUtil.getJSONObjectMapper().writeValueAsString(expectedJson);
+
+		Assert.assertEquals(finalExpected, actual);
+	}
+
+	@Test
 	public void testFilterEndToEndWithMorphScriptBuilder() throws Exception {
 
 		final String expected = DMPPersistenceUtil.getResourceAsString("test-mabxml.filter.morphscript.result.json");
@@ -124,93 +143,106 @@ public class FilterTransformationFlowTest extends GuicedTest {
 		Assert.assertEquals(finalExpected, actual);
 	}
 
-	private ArrayNode replaceKeyWithActualKey(final String expected, final String actual) throws JsonParseException, JsonMappingException,
-			IOException {
+	private ArrayNode replaceKeyWithActualKey(final String expected, final String actual) throws IOException {
 
 		// replace key with actual key
 		final ArrayNode expectedJson = DMPPersistenceUtil.getJSONObjectMapper().readValue(expected, ArrayNode.class);
-		final ObjectNode expectedTuple = (ObjectNode) expectedJson.get(0);
-		final String expectedFieldName = expectedTuple.fieldNames().next();
-		final ArrayNode expectedContent = (ArrayNode) expectedTuple.get(expectedFieldName);
-		ObjectNode expectedContentJson = null;
-		JsonNode typeNode = null;
 
-		for (final JsonNode expectedContentJsonCandidate : expectedContent) {
+		final Iterator<JsonNode> iter = expectedJson.iterator();
+		int iterCount = 0;
 
-			final String expectedContentJsonFieldName = expectedContentJsonCandidate.fieldNames().next();
+		while (iter.hasNext()) {
 
-			if (expectedContentJsonFieldName.equals("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")) {
+			final ObjectNode expectedTuple = (ObjectNode) iter.next();
+			final String expectedFieldName = expectedTuple.fieldNames().next();
+			final ArrayNode expectedContent = (ArrayNode) expectedTuple.get(expectedFieldName);
+			ObjectNode expectedContentJson = null;
+			JsonNode typeNode = null;
 
-				typeNode = expectedContentJsonCandidate;
+			for (final JsonNode expectedContentJsonCandidate : expectedContent) {
+
+				final String expectedContentJsonFieldName = expectedContentJsonCandidate.fieldNames().next();
+
+				if (expectedContentJsonFieldName.equals("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")) {
+
+					typeNode = expectedContentJsonCandidate;
+
+					continue;
+				}
+
+				expectedContentJson = (ObjectNode) expectedContentJsonCandidate;
+			}
+
+			if (expectedContentJson == null) {
 
 				continue;
 			}
 
-			expectedContentJson = (ObjectNode) expectedContentJsonCandidate;
-		}
+			Assert.assertNotNull("expected content JSON shouldn't be null", expectedContentJson);
 
-		Assert.assertNotNull("expected content JSON shouldn't be null", expectedContentJson);
+			final JsonNode expectedContentValue = expectedContentJson.get(expectedContentJson.fieldNames().next());
 
-		final JsonNode expectedContentValue = expectedContentJson.get(expectedContentJson.fieldNames().next());
+			Assert.assertNotNull("the actual transformation result shouldn't be null", actual);
+			final ArrayNode actualJson = DMPPersistenceUtil.getJSONObjectMapper().readValue(actual, ArrayNode.class);
+			Assert.assertNotNull("the deserialised JSON array of the actual transformation result shouldn't be null", actualJson);
+			final ObjectNode actualTuple = (ObjectNode) actualJson.get(iterCount);
+			Assert.assertNotNull("the tuple of the deserialised JSON array of the actual transformation result shouldn't be null", actualTuple);
+			final JsonNode actualContent = actualTuple.get(actualTuple.fieldNames().next());
+			Assert.assertNotNull("the content of the tuple of the deserialised JSON array of the actual transformation result shouldn't be null",
+					actualContent);
+			Assert.assertTrue("the actual content should be a JSON array", actualContent.isArray());
+			JsonNode actualContentJson = null;
+			Integer typeNodePosition = null;
+			int i = 0;
 
-		Assert.assertNotNull("the actual transformation result shouldn't be null", actual);
-		final ArrayNode actualJson = DMPPersistenceUtil.getJSONObjectMapper().readValue(actual, ArrayNode.class);
-		Assert.assertNotNull("the deserialised JSON array of the actual transformation result shouldn't be null", actualJson);
-		final ObjectNode actualTuple = (ObjectNode) actualJson.get(0);
-		Assert.assertNotNull("the tuple of the deserialised JSON array of the actual transformation result shouldn't be null", actualTuple);
-		final JsonNode actualContent = actualTuple.get(actualTuple.fieldNames().next());
-		Assert.assertNotNull("the content of the tuple of the deserialised JSON array of the actual transformation result shouldn't be null",
-				actualContent);
-		Assert.assertTrue("the actual content should be a JSON array", actualContent.isArray());
-		JsonNode actualContentJson = null;
-		Integer typeNodePosition = null;
-		int i = 0;
+			for (final JsonNode actualContentJsonCandidate : actualContent) {
 
-		for (final JsonNode actualContentJsonCandidate : actualContent) {
+				i++;
 
-			i++;
+				final String actualContentJsonFieldName = actualContentJsonCandidate.fieldNames().next();
 
-			final String actualContentJsonFieldName = actualContentJsonCandidate.fieldNames().next();
+				if (actualContentJsonFieldName.equals("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")) {
 
-			if (actualContentJsonFieldName.equals("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")) {
+					typeNodePosition = i;
 
-				typeNodePosition = i;
+					continue;
+				}
 
-				continue;
+				actualContentJson = actualContentJsonCandidate;
 			}
 
-			actualContentJson = actualContentJsonCandidate;
-		}
+			Assert.assertNotNull("the actual content JSON shouldn't be null", actualContentJson);
+			Assert.assertTrue("the actual content JSON should be a JSON object", actualContentJson.isObject());
 
-		Assert.assertNotNull("the actual content JSON shouldn't be null", actualContentJson);
-		Assert.assertTrue("the actual content JSON should be a JSON object", actualContentJson.isObject());
+			final ObjectNode newExpectedContentJson = DMPPersistenceUtil.getJSONObjectMapper().createObjectNode();
+			newExpectedContentJson.put(actualContentJson.fieldNames().next(), expectedContentValue);
+			final ArrayNode newExpectedContent = DMPPersistenceUtil.getJSONObjectMapper().createArrayNode();
 
-		final ObjectNode newExpectedContentJson = DMPPersistenceUtil.getJSONObjectMapper().createObjectNode();
-		newExpectedContentJson.put(actualContentJson.fieldNames().next(), expectedContentValue);
-		final ArrayNode newExpectedContent = DMPPersistenceUtil.getJSONObjectMapper().createArrayNode();
+			if (typeNode != null) {
 
-		if (typeNode != null) {
+				if (typeNodePosition != null) {
 
-			if (typeNodePosition != null) {
+					if (typeNodePosition == 1) {
 
-				if (typeNodePosition == 1) {
+						newExpectedContent.add(typeNode);
+						newExpectedContent.add(newExpectedContentJson);
+					} else {
 
-					newExpectedContent.add(typeNode);
-					newExpectedContent.add(newExpectedContentJson);
+						newExpectedContent.add(newExpectedContentJson);
+						newExpectedContent.add(typeNode);
+					}
 				} else {
 
 					newExpectedContent.add(newExpectedContentJson);
-					newExpectedContent.add(typeNode);
 				}
 			} else {
 
 				newExpectedContent.add(newExpectedContentJson);
 			}
-		} else {
+			expectedTuple.put(expectedFieldName, newExpectedContent);
 
-			newExpectedContent.add(newExpectedContentJson);
+			iterCount++;
 		}
-		expectedTuple.put(expectedFieldName, newExpectedContent);
 
 		return expectedJson;
 	}
