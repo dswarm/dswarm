710c755237122b50224ada5c7ec49b932065005a
Thomas Gängler <tgaengler@avantgarde-labs.de>
2014-11-11 16:07:18 +0100

    [!][DD-835] fixed internal data provisioning generation (metadata repository dumping) + re-generated init script in linux (to guarantee case sensitivity)

--- init/src/main/java/org/dswarm/init/util/CmdUtil.java
+++ init/src/main/java/org/dswarm/init/util/CmdUtil.java
@@ -3,32 +3,44 @@ package org.dswarm.init.util;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.collect.Lists;
+
+import org.dswarm.init.DMPException;
+
 public class CmdUtil {
 
-	private static final Logger LOG = LoggerFactory.getLogger(CmdUtil.class);
-	
+	private static final Logger	LOG	= LoggerFactory.getLogger(CmdUtil.class);
+
 	public static String executeCommand(final String command) throws Exception {
 		final Process process = Runtime.getRuntime().exec(command);
-		return executeProcess( process, command );
+		return CmdUtil.executeProcess(process, command);
 	}
 
-	public static String executeCommand(final String command, final String[] envp ) throws Exception {
+	public static String executeCommand(final String command, final String[] envp) throws Exception {
 		final Process process = Runtime.getRuntime().exec(command, envp);
-		return executeProcess(process, command);
+		return CmdUtil.executeProcess(process, command);
 	}
 
-	
-	private static String executeProcess( Process process, String command ) throws InterruptedException, IOException {
+	private static String executeProcess(final Process process, final String command) throws InterruptedException, IOException, DMPException {
+
 		final int exitStatus = process.waitFor();
 
-				final StringBuilder sb = new StringBuilder();
+		if (exitStatus != 0) {
+
+			throw new DMPException("couldn't execute command '" + command + "' sucessfully - exit status is '" + exitStatus + "'");
+		}
+
+		final StringBuilder sb = new StringBuilder();
 
 		final BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 		String line = reader.readLine();
@@ -37,52 +49,81 @@ public class CmdUtil {
 			line = reader.readLine();
 		}
 
-		LOG.debug("got result from command execution '" + command + "' = '" + sb.toString() + "'");
+		CmdUtil.LOG.debug("got result from command execution '" + command + "' = '" + sb.toString() + "'");
 
 		return sb.toString();
 	}
-	
-	
-	public static void runCommand( final String command, final String output ) {
+
+	public static void runCommand(final String command, final String output) throws Exception {
 		String[] osSpecificCommand;
-		if( System.getenv("OS") != null && System.getenv("OS").startsWith("Windows") ) {
-			osSpecificCommand = new String[] { "cmd.exe", "/c", command };				
+		if (System.getenv("OS") != null && System.getenv("OS").startsWith("Windows")) {
+			osSpecificCommand = new String[] { "cmd.exe", "/c", command };
+
+			try {
+				CmdUtil.runProcess(osSpecificCommand, output);
+			} catch (IOException | InterruptedException e) {
+				CmdUtil.LOG.error(e.getMessage(), e);
+			}
 		} else {
-			osSpecificCommand = new String[] { command };	
-		}
-		
-		try {
-			runProcess( osSpecificCommand, output );
-		} catch( IOException | InterruptedException e ) {
-			LOG.error(e.getMessage(), e);
+
+			final Process process = Runtime.getRuntime().exec(command);
+
+			final int exitStatus = process.waitFor();
+
+			if (exitStatus != 0) {
+
+				throw new DMPException("couldn't execute command '" + command + "' sucessfully - exit status is '" + exitStatus + "'");
+			}
+
+			final BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+			final List<String> lines = Lists.newArrayList();
+
+			String line = reader.readLine();
+
+			while (line != null) {
+
+				lines.add(line);
+				line = reader.readLine();
+			}
+
+			FileUtils.writeLines(new File(output), "UTF-8", lines);
 		}
 	}
-	
-	
-	private static void runProcess( final String[] command, final String output ) throws IOException, InterruptedException {
+
+	/**
+	 * [@tgaengler]: note, it looks like that this only works for windows machines
+	 * 
+	 * @param command
+	 * @param output
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	private static void runProcess(final String[] command, final String output) throws IOException, InterruptedException {
 		final Process process = Runtime.getRuntime().exec(command);
-		
-		if( process != null ) {
+
+		if (process != null) {
 			new Thread(new Runnable() {
+
 				@Override
 				public void run() {
 					try {
 						try (BufferedReader reader = new BufferedReader(new InputStreamReader(new DataInputStream(process.getInputStream())));
 								BufferedWriter writer = new BufferedWriter(new FileWriter(output))) {
 							String line;
-							while( (line = reader.readLine()) != null ) {
+							while ((line = reader.readLine()) != null) {
 								writer.write(line);
 								writer.newLine();
 							}
 						}
-					} catch( Exception ex ) {
-						LOG.error(ex.getMessage(), ex);
+					} catch (final Exception ex) {
+						CmdUtil.LOG.error(ex.getMessage(), ex);
 					}
 				}
 			}).start();
 		}
-		
-		if( process != null && process.waitFor() == 0 ) {
+
+		if (process != null && process.waitFor() == 0) {
 			// success ...
 		} else {
 			// failed
