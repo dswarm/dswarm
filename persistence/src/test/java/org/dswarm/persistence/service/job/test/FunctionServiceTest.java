504a8086cfd157fae0998708e42a519c71c7ae5e
Thomas Gängler <tgaengler@avantgarde-labs.de>
2014-11-12 17:21:31 +0100

    [WIP][DD-833] finalized FunctionServiceTest re-factoring

--- persistence/src/test/java/org/dswarm/persistence/service/job/test/FunctionServiceTest.java
+++ persistence/src/test/java/org/dswarm/persistence/service/job/test/FunctionServiceTest.java
@@ -34,67 +34,38 @@ import org.slf4j.LoggerFactory;
 
 import org.dswarm.persistence.GuicedTest;
 import org.dswarm.persistence.model.job.Function;
-import org.dswarm.persistence.model.job.FunctionType;
 import org.dswarm.persistence.model.job.proxy.ProxyFunction;
 import org.dswarm.persistence.service.job.FunctionService;
+import org.dswarm.persistence.service.job.test.utils.FunctionServiceTestUtils;
 import org.dswarm.persistence.service.test.IDBasicJPAServiceTest;
 import org.dswarm.persistence.util.DMPPersistenceUtil;
 
 public class FunctionServiceTest extends IDBasicJPAServiceTest<ProxyFunction, Function, FunctionService> {
 
-	private static final Logger	LOG				= LoggerFactory.getLogger(FunctionServiceTest.class);
+	private static final Logger LOG = LoggerFactory.getLogger(FunctionServiceTest.class);
 
-	private final ObjectMapper	objectMapper	= GuicedTest.injector.getInstance(ObjectMapper.class);
+	private final ObjectMapper objectMapper = GuicedTest.injector.getInstance(ObjectMapper.class);
+
+	private FunctionServiceTestUtils functionServiceTestUtils;
 
 	public FunctionServiceTest() {
 
 		super("function", FunctionService.class);
 	}
 
+	@Override protected void initObjects() {
+
+		super.initObjects();
+
+		functionServiceTestUtils = new FunctionServiceTestUtils();
+	}
+
 	@Test
 	public void testSimpleFunction() throws Exception {
 
-		final String functionName = "trim";
-		final String functionDescription = "trims leading and trailing whitespaces from a given string";
-		final String functionParameter1 = "inputString";
-		final String functionParameter2 = "parameter2";
-
-		final String functionFunctionDescriptionString = DMPPersistenceUtil.getResourceAsString("function_description.prettyprint.json");
-
-		Assert.assertNotNull("the function description JSON string shouldn't be null", functionFunctionDescriptionString);
-
-		final ObjectNode functionFunctionDescription = objectMapper.readValue(functionFunctionDescriptionString, ObjectNode.class);
-
-		Assert.assertNotNull("the function description JSON shouldn't be null", functionFunctionDescription);
-
-		final Function function = createObject().getObject();
-		function.setName(functionName);
-		function.setDescription(functionDescription);
-		function.addParameter(functionParameter1);
-		function.addParameter(functionParameter2);
-		function.setFunctionDescription(functionFunctionDescription);
+		final Function function = functionServiceTestUtils.createDefaultObject();
 
 		final Function updatedFunction = updateObjectTransactional(function).getObject();
-
-		Assert.assertNotNull("the function name shouldn't be null", updatedFunction.getName());
-		Assert.assertEquals("the function names are not equal", functionName, updatedFunction.getName());
-		Assert.assertNotNull("the function description shouldn't be null", updatedFunction.getDescription());
-		Assert.assertEquals("the function descriptions are not equal", functionDescription, updatedFunction.getDescription());
-
-		Assert.assertNotNull("the function description JSON shouldn't be null", updatedFunction.getFunctionDescription());
-
-		final String functionDescriptionJSONString = objectMapper.writeValueAsString(updatedFunction.getFunctionDescription());
-
-		final String functionFunctionDescriptionJSONString = objectMapper.writeValueAsString(functionFunctionDescription);
-
-		Assert.assertEquals("the function description JSON strings are not equal", functionFunctionDescriptionJSONString,
-				functionDescriptionJSONString);
-
-		Assert.assertNotNull("the function parameters shouldn't be null", updatedFunction.getParameters());
-		Assert.assertEquals("the function parameters' size are not equal", 2, updatedFunction.getParameters().size());
-		Assert.assertEquals("the function parameter '" + functionParameter1 + "' are not equal", functionParameter1, updatedFunction.getParameters()
-				.get(0));
-		Assert.assertEquals("the function type is not '" + FunctionType.Function + "'", FunctionType.Function, updatedFunction.getFunctionType());
 
 		String json = null;
 
@@ -106,9 +77,6 @@ public class FunctionServiceTest extends IDBasicJPAServiceTest<ProxyFunction, Fu
 		}
 
 		FunctionServiceTest.LOG.debug("function json: " + json);
-
-		// clean up DB
-		deleteObject(function.getId());
 	}
 
 	// @Test
--- persistence/src/test/java/org/dswarm/persistence/service/job/test/utils/FunctionServiceTestUtils.java
+++ persistence/src/test/java/org/dswarm/persistence/service/job/test/utils/FunctionServiceTestUtils.java
@@ -15,15 +15,54 @@
  */
 package org.dswarm.persistence.service.job.test.utils;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.Assert;
+
 import org.dswarm.persistence.model.job.Function;
 import org.dswarm.persistence.model.job.proxy.ProxyFunction;
 import org.dswarm.persistence.service.job.FunctionService;
+import org.dswarm.persistence.util.DMPPersistenceUtil;
 
 public class FunctionServiceTestUtils extends BasicFunctionServiceTestUtils<FunctionService, ProxyFunction, Function> {
 
 	public FunctionServiceTestUtils() {
 
 		super(Function.class, FunctionService.class);
+	}
+
+	@Override public Function createObject(JsonNode objectDescription) throws Exception {
+		return null;
+	}
+
+	@Override public Function createObject(String identifier) throws Exception {
+		return null;
+	}
+
+	@Override public Function createDefaultObject() throws Exception {
+
+		final String functionName = "trim";
+		final String functionDescription = "trims leading and trailing whitespaces from a given string";
+		final String functionParameter1 = "inputString";
+		final String functionParameter2 = "parameter2";
+
+		final String functionFunctionDescriptionString = DMPPersistenceUtil.getResourceAsString("function_description.prettyprint.json");
+
+		Assert.assertNotNull("the function description JSON string shouldn't be null", functionFunctionDescriptionString);
+
+		final ObjectNode functionFunctionDescription = objectMapper.readValue(functionFunctionDescriptionString, ObjectNode.class);
+
+		Assert.assertNotNull("the function description JSON shouldn't be null", functionFunctionDescription);
+
+		final Function function = new Function();
+
+		function.setName(functionName);
+		function.setDescription(functionDescription);
+		function.addParameter(functionParameter1);
+		function.addParameter(functionParameter2);
+		function.setFunctionDescription(functionFunctionDescription);
+
+		return createAndCompareObject(function, function);
 	}
 
 	@Override
