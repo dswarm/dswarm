b7a2e738590c501c5e5e8f6315dc99d940cf0d9b
Thomas Gängler <tgaengler@avantgarde-labs.de>
2014-11-26 17:35:30 +0100

    [WIP][DD-833] fixed another ProjectsResource test failure

--- persistence/src/test/java/org/dswarm/persistence/service/test/utils/BasicJPAServiceTestUtils.java
+++ persistence/src/test/java/org/dswarm/persistence/service/test/utils/BasicJPAServiceTestUtils.java
@@ -26,6 +26,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import org.json.JSONException;
 import org.junit.Assert;
 import org.slf4j.Logger;
@@ -73,17 +74,20 @@ public abstract class BasicJPAServiceTestUtils<POJOCLASSPERSISTENCESERVICE exten
 		objectMapper = GuicedTest.injector.getInstance(ObjectMapper.class);
 	}
 
-	@Override public void prepare() throws Exception {
+	@Override
+	public void prepare() throws Exception {
 
 		// this should be done by the concrete test, not in the utils
 	}
 
-	@Override public void tearDown3() throws Exception {
+	@Override
+	public void tearDown3() throws Exception {
 
 		// this should be done by the concrete test, not in the utils
 	}
 
-	@Override public void testSimpleObject() throws Exception {
+	@Override
+	public void testSimpleObject() throws Exception {
 
 		// this should be implemented in real test classes, not in the utils
 	}
@@ -109,18 +113,32 @@ public abstract class BasicJPAServiceTestUtils<POJOCLASSPERSISTENCESERVICE exten
 
 		Assert.assertNotNull("response " + pojoClassName + "s JSON array shouldn't be null", responseObjectsJSONArray);
 
-		final Iterator<JsonNode> responseObjectsJSONIter = responseObjectsJSONArray.iterator();
-
-		while (responseObjectsJSONIter.hasNext()) {
-
-			final JsonNode responseObjectJSON = responseObjectsJSONIter.next();
+		for (final JsonNode responseObjectJSON : responseObjectsJSONArray) {
 
 			final POJOCLASS responseObject = objectMapper.readValue(responseObjectJSON.toString(), pojoClass);
 
 			responseObjects.put(responseObject.getId(), responseObject);
 		}
 
-		compareObjects(expectedObjects, responseObjects);
+		final Set<POJOCLASS> newExpectedObjects;
+
+		if (expectedObjects != null) {
+
+			newExpectedObjects = Sets.newLinkedHashSet();
+
+			for (final POJOCLASS expectedObject : expectedObjects) {
+
+				final String expectedObjectJSONString = objectMapper.writeValueAsString(expectedObject);
+				final POJOCLASS newExpectedObject = objectMapper.readValue(expectedObjectJSONString, pojoClass);
+
+				newExpectedObjects.add(newExpectedObject);
+			}
+		} else {
+
+			newExpectedObjects = expectedObjects;
+		}
+
+		compareObjects(newExpectedObjects, responseObjects);
 	}
 
 	/**
