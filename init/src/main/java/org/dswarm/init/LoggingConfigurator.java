13a8e280d3e97735542edbb5fa7f2923b175ee83
Thomas Gängler <tgaengler@avantgarde-labs.de>
2014-12-15 10:58:16 +0100

    [DD-886] separated affected tests (+ decreased logging level)

--- init/src/main/java/org/dswarm/init/LoggingConfigurator.java
+++ init/src/main/java/org/dswarm/init/LoggingConfigurator.java
@@ -79,7 +79,9 @@ public final class LoggingConfigurator {
 	private static void configureLogback(final String loggingPath, final LoggerContext lc) {
 
 		final Logger dswarmLogger = lc.getLogger("org.dswarm");
-		dswarmLogger.setLevel(Level.DEBUG);
+		// TODO: decrease/increase logging level as necessary
+		// TODO: make logging level configurable via Typesafe config
+		// dswarmLogger.setLevel(Level.DEBUG);
 		dswarmLogger.addAppender(addFileAppender(lc, loggingPath, Level.DEBUG));
 		dswarmLogger.addAppender(addFileAppender(lc, loggingPath, Level.TRACE));
 		dswarmLogger.addAppender(addFileAppender(lc, loggingPath, Level.INFO));
