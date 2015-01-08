a49acef41bc1bf204aea8d7e82ead6979b5beb5d
Paul Horn <phorn@avantgarde-labs.de>
2015-01-08 14:25:18 +0100

    [DD-898] Reformat/-structure Hibernate configuration

--- persistence/src/main/java/org/dswarm/persistence/JpaHibernateModule.java
+++ persistence/src/main/java/org/dswarm/persistence/JpaHibernateModule.java
@@ -58,33 +58,34 @@ public class JpaHibernateModule extends AbstractModule {
 		properties.setProperty("javax.persistence.jdbc.password", password);
 
 		// TODO: load more values from config, e.g. connection pool settings
-		properties.setProperty("javax.persistence.jdbc.driver", "com.mysql.jdbc.Driver");
-		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
-		properties.setProperty("hibernate.connection.shutdown", "true");
-		properties.setProperty("hibernate.show_sql", String.valueOf(isLogSql));
-		properties.setProperty("hibernate.cache.region.factory_class", "org.hibernate.cache.internal.NoCachingRegionFactory");
-		properties.setProperty("hibernate.cache.use_second_level_cache", "false");
-		properties.setProperty("hibernate.cache.use_query_cache", "false");
-		properties.setProperty("hibernate.connection.provider_class", "org.hibernate.service.jdbc.connections.internal.C3P0ConnectionProvider");
-		properties.setProperty("hibernate.c3p0.max_size", "20");
-		properties.setProperty("hibernate.c3p0.min_size", "5");
-		properties.setProperty("hibernate.c3p0.idle_test_period", "30");
-		properties.setProperty("hibernate.c3p0.max_statements", "0");
-		properties.setProperty("hibernate.c3p0.maxStatementsPerConnection", "20");
-		properties.setProperty("hibernate.c3p0.timeout", "10");
-		properties.setProperty("hibernate.c3p0.maxIdleTimeExcessConnections", "45");
-		properties.setProperty("hibernate.c3p0.maxConnectionAge", "500");
-		properties.setProperty("hibernate.c3p0.numHelperThreads", "1");
-		properties.setProperty("hibernate.c3p0.checkoutTimeout", "0");
-		properties.setProperty("hibernate.c3p0.testConnectionOnCheckout", "true");
-		properties.setProperty("hibernate.c3p0.preferredTestQuery", "SELECT 1");
 		properties.setProperty("hibernate.c3p0.acquireRetryAttempts", "3");
 		properties.setProperty("hibernate.c3p0.acquireRetryDelay", "100");
 		properties.setProperty("hibernate.c3p0.breakAfterAcquireFailure", "false");
-		properties.setProperty("hibernate.current_session_context_class", "thread");
+		properties.setProperty("hibernate.c3p0.checkoutTimeout", "0");
+		properties.setProperty("hibernate.c3p0.idle_test_period", "30");
+		properties.setProperty("hibernate.c3p0.max_size", "20");
+		properties.setProperty("hibernate.c3p0.max_statements", "0");
+		properties.setProperty("hibernate.c3p0.maxConnectionAge", "500");
+		properties.setProperty("hibernate.c3p0.maxIdleTimeExcessConnections", "45");
+		properties.setProperty("hibernate.c3p0.maxStatementsPerConnection", "20");
+		properties.setProperty("hibernate.c3p0.min_size", "5");
+		properties.setProperty("hibernate.c3p0.numHelperThreads", "1");
+		properties.setProperty("hibernate.c3p0.preferredTestQuery", "SELECT 1");
+		properties.setProperty("hibernate.c3p0.testConnectionOnCheckout", "true");
+		properties.setProperty("hibernate.c3p0.timeout", "10");
+		properties.setProperty("hibernate.cache.region.factory_class", "org.hibernate.cache.internal.NoCachingRegionFactory");
+		properties.setProperty("hibernate.cache.use_query_cache", "false");
+		properties.setProperty("hibernate.cache.use_second_level_cache", "false");
 		properties.setProperty("hibernate.connection.autoReconnect", "true");
 		properties.setProperty("hibernate.connection.autoReconnectForPools", "true");
 		properties.setProperty("hibernate.connection.is-connection-validation-required", "true");
+		properties.setProperty("hibernate.connection.provider_class", "org.hibernate.service.jdbc.connections.internal.C3P0ConnectionProvider");
+		properties.setProperty("hibernate.connection.shutdown", "true");
+		properties.setProperty("hibernate.current_session_context_class", "thread");
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+		properties.setProperty("hibernate.discriminator.ignore_explicit_for_joined", "true");
+		properties.setProperty("hibernate.show_sql", String.valueOf(isLogSql));
+		properties.setProperty("javax.persistence.jdbc.driver", "com.mysql.jdbc.Driver");
 
 		return properties;
 	}
