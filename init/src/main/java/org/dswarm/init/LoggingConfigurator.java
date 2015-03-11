c4ba1d2a754f7b448176ffa83e1e6884622e9879
Paul Horn <phorn@avantgarde-labs.de>
2015-03-11 09:59:02 +0100

    [DD-958] Refactor logging configuration

--- init/src/main/java/org/dswarm/init/LoggingConfigurator.java
+++ init/src/main/java/org/dswarm/init/LoggingConfigurator.java
@@ -15,7 +15,7 @@
  */
 package org.dswarm.init;
 
-
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import ch.qos.logback.classic.Level;
@@ -25,6 +25,7 @@ import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
 import ch.qos.logback.classic.filter.ThresholdFilter;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.Appender;
+import ch.qos.logback.core.Context;
 import ch.qos.logback.core.encoder.Encoder;
 import ch.qos.logback.core.filter.Filter;
 import ch.qos.logback.core.rolling.RollingFileAppender;
@@ -33,7 +34,6 @@ import ch.qos.logback.core.rolling.SizeAndTimeBasedFNATP;
 import ch.qos.logback.core.rolling.TimeBasedFileNamingAndTriggeringPolicy;
 import ch.qos.logback.core.rolling.TimeBasedRollingPolicy;
 import ch.qos.logback.core.status.InfoStatus;
-import ch.qos.logback.core.status.OnConsoleStatusListener;
 import ch.qos.logback.core.status.StatusManager;
 import com.google.common.base.Joiner;
 import com.google.common.base.Preconditions;
@@ -45,22 +45,35 @@ public final class LoggingConfigurator {
 
 	private static final String FILE_SEPARATOR = System.getProperty("file.separator", "/");
 	private static final String LOGGING_PATH_CONFIG = "dswarm.paths.logging";
-	private static final String LOGLEVEL_CONFIG = "dswarm.loglevel";
-	private static final String ROOT_LOGLEVEL_CONFIG = "dswarm.root-loglevel";
+	private static final String DEPRECATED_CONFIG_ROOT = "dswarm.";
+	private static final String LOGGING_CONFIG_ROOT = "dswarm.logging.";
+	private static final String LOGLEVEL_CONFIG = "loglevel";
+	private static final String ROOT_LOGLEVEL_CONFIG = "root-loglevel";
+	private static final String TO_CONSOLE_CONFIG = "dswarm.logging.log-to-console";
 
 	private static final AtomicBoolean ALREADY_CONFIGURED = new AtomicBoolean(false);
 
-	private LoggingConfigurator() {}
+	private LoggingConfigurator() {
+	}
 
 	public static void configureFrom(final Config config) {
 		Preconditions.checkNotNull(config);
+		final String loggingPath = config.getString(LOGGING_PATH_CONFIG);
+		final Optional<String> logLevel = ConfigModule.firstString(config,
+				LOGGING_CONFIG_ROOT + LOGLEVEL_CONFIG,
+				DEPRECATED_CONFIG_ROOT + LOGLEVEL_CONFIG);
+		final Optional<String> rootLogLevel = ConfigModule.firstString(config,
+				LOGGING_CONFIG_ROOT + ROOT_LOGLEVEL_CONFIG,
+				DEPRECATED_CONFIG_ROOT + ROOT_LOGLEVEL_CONFIG);
+		final boolean resetLoggingContext = !config.getBoolean(TO_CONSOLE_CONFIG);
 		configure(
-				config.getString(LOGGING_PATH_CONFIG),
-				config.getString(ROOT_LOGLEVEL_CONFIG),
-				config.getString(LOGLEVEL_CONFIG));
+				loggingPath,
+				logLevel.orElse("default"),
+				rootLogLevel.orElse("default"),
+				resetLoggingContext);
 	}
 
-	private static void configure(final String loggingPath, final String logLevel, final String rootLogLevel) {
+	private static void configure(final String loggingPath, final String logLevel, final String rootLogLevel, final boolean resetLoggingContext) {
 		if (ALREADY_CONFIGURED.getAndSet(true)) {
 			return;
 		}
@@ -71,8 +84,12 @@ public final class LoggingConfigurator {
 		}
 
 		final StatusManager sm = lc.getStatusManager();
-		if (sm != null)  {
+		if (sm != null) {
 			sm.add(new InfoStatus("Setting up dswarm configuration.", lc));
+		}
+
+		if (resetLoggingContext) {
+			lc.reset();
 		}
 
 		final Level level = Level.toLevel(logLevel, Level.INFO);
@@ -87,7 +104,6 @@ public final class LoggingConfigurator {
 	private static void configureLogback(final String loggingPath, final LoggerContext lc, final Level logLevel, final Level rootLogLevel) {
 		configureDswarmLogger(loggingPath, lc, logLevel);
 		configureRootLogger(loggingPath, lc, rootLogLevel);
-		OnConsoleStatusListener.addNewInstanceToContext(lc);
 	}
 
 	private static void configureDswarmLogger(final String loggingPath, final LoggerContext lc, final Level logLevel) {
@@ -109,21 +125,21 @@ public final class LoggingConfigurator {
 		rootLogger.addAppender(addFileAppender(lc, loggingPath, "default", Level.ERROR));
 	}
 
-	private static Appender<ILoggingEvent> addFileAppender(final LoggerContext lc, final String loggingPath, final Level level) {
+	private static Appender<ILoggingEvent> addFileAppender(final Context lc, final String loggingPath, final Level level) {
 		final String basePath = filePath(loggingPath, "dmp", level.levelStr.toLowerCase());
 		final String name = String.format("FA%s", level.levelStr.charAt(0));
 
 		return getFileAppender(lc, level, basePath, name);
 	}
 
-	private static Appender<ILoggingEvent> addFileAppender(final LoggerContext lc, final String loggingPath, final String alternative, final Level level) {
+	private static Appender<ILoggingEvent> addFileAppender(final Context lc, final String loggingPath, final String alternative, final Level level) {
 		final String basePath = filePath(loggingPath, alternative, level.levelStr.toLowerCase());
 		final String name = alternative.toUpperCase();
 
 		return getFileAppender(lc, level, basePath, name);
 	}
 
-	private static Appender<ILoggingEvent> getFileAppender(final LoggerContext lc, final Level level, final String basePath, final String name) {
+	private static Appender<ILoggingEvent> getFileAppender(final Context lc, final Level level, final String basePath, final String name) {
 		final RollingFileAppender<ILoggingEvent> fileAppender = createRollingFileAppender(lc, name, basePath);
 
 		final RollingPolicy rollingPolicy = getRollingPolicy(lc, basePath, getTriggeringPolicy(lc));
@@ -137,7 +153,7 @@ public final class LoggingConfigurator {
 		return fileAppender;
 	}
 
-	private static RollingFileAppender<ILoggingEvent> createRollingFileAppender(final LoggerContext lc, final String name, final String basePath) {
+	private static RollingFileAppender<ILoggingEvent> createRollingFileAppender(final Context lc, final String name, final String basePath) {
 		final RollingFileAppender<ILoggingEvent> traceFileAppender = new RollingFileAppender<>();
 		traceFileAppender.setContext(lc);
 		traceFileAppender.setName(name);
@@ -145,14 +161,14 @@ public final class LoggingConfigurator {
 		return traceFileAppender;
 	}
 
-	private static TimeBasedFileNamingAndTriggeringPolicy<ILoggingEvent> getTriggeringPolicy(final LoggerContext lc) {
+	private static TimeBasedFileNamingAndTriggeringPolicy<ILoggingEvent> getTriggeringPolicy(final Context lc) {
 		final SizeAndTimeBasedFNATP<ILoggingEvent> fnatp = new SizeAndTimeBasedFNATP<>();
 		fnatp.setContext(lc);
 		fnatp.setMaxFileSize("50MB");
 		return fnatp;
 	}
 
-	private static RollingPolicy getRollingPolicy(final LoggerContext lc, final String basePath, final TimeBasedFileNamingAndTriggeringPolicy<ILoggingEvent> triggeringPolicy) {
+	private static RollingPolicy getRollingPolicy(final Context lc, final String basePath, final TimeBasedFileNamingAndTriggeringPolicy<ILoggingEvent> triggeringPolicy) {
 		final TimeBasedRollingPolicy<ILoggingEvent> rollingPolicy = new TimeBasedRollingPolicy<>();
 		rollingPolicy.setContext(lc);
 		rollingPolicy.setFileNamePattern(filePath(basePath, "messages.%d{yyyy-MM-dd}.%i.log"));
@@ -161,7 +177,7 @@ public final class LoggingConfigurator {
 		return rollingPolicy;
 	}
 
-	private static Filter<ILoggingEvent> getFilter(final LoggerContext lc, final Level level) {
+	private static Filter<ILoggingEvent> getFilter(final Context lc, final Level level) {
 		final ThresholdFilter filter = new ThresholdFilter();
 		filter.setContext(lc);
 		filter.setLevel(level.levelStr);
@@ -169,7 +185,7 @@ public final class LoggingConfigurator {
 		return filter;
 	}
 
-	private static Encoder<ILoggingEvent> getEncoder(final LoggerContext lc) {
+	private static Encoder<ILoggingEvent> getEncoder(final Context lc) {
 		final PatternLayoutEncoder encoder = new PatternLayoutEncoder();
 		encoder.setContext(lc);
 		encoder.setPattern("%date %level [%thread] %logger [%file:%line] %msg%n");
