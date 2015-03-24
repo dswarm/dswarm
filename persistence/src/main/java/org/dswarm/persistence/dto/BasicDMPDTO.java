d59bce8d1133b0d7fcd34d4f9dba6a2931775ef2
Paul Horn <phorn@avantgarde-labs.de>
2015-03-24 11:25:20 +0100

    [DD-1050] Add different DTOs for different response variants

---
+++ persistence/src/main/java/org/dswarm/persistence/dto/BasicDMPDTO.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (C) 2013 – 2015 SLUB Dresden & Avantgarde Labs GmbH (<code@dswarm.org>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.dswarm.persistence.dto;
+
+import java.net.URI;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public abstract class BasicDMPDTO<SELF extends BasicDMPDTO<SELF>> {
+	@JsonProperty("uuid")
+	public final String uuid;
+
+	@JsonProperty("name")
+	public final String name;
+
+	@JsonProperty("description")
+	public final String description;
+
+	@JsonProperty("_href")
+	public final String href;
+
+	protected BasicDMPDTO(
+			final String uuid,
+			final String name,
+			final String description,
+			final String href) {
+		this.uuid = uuid;
+		this.name = name;
+		this.description = description;
+		this.href = href;
+	}
+
+	public final SELF withHref(final URI objectURI) {
+		final String uriString = objectURI == null ? null : objectURI.toString();
+		return copyWithHref(uriString);
+	}
+
+	@Override
+	public final String toString() {
+		return ReflectionToStringBuilder.toString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+	}
+
+	protected abstract SELF copyWithHref(final String objectURI);
+}
---
+++ persistence/src/main/java/org/dswarm/persistence/dto/ShortExtendendBasicDMPDTO.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (C) 2013 – 2015 SLUB Dresden & Avantgarde Labs GmbH (<code@dswarm.org>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.dswarm.persistence.dto;
+
+import java.net.URI;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import org.dswarm.persistence.model.ExtendedBasicDMPJPAObject;
+
+public final class ShortExtendendBasicDMPDTO extends BasicDMPDTO<ShortExtendendBasicDMPDTO> {
+
+	@JsonCreator
+	public ShortExtendendBasicDMPDTO(
+			@JsonProperty("uuid")
+			final String uuid,
+			@JsonProperty("name")
+			final String name,
+			@JsonProperty("description")
+			final String description,
+			@JsonProperty("_href")
+			final String href) {
+		super(uuid, name, description, href);
+	}
+
+	@Override
+	protected ShortExtendendBasicDMPDTO copyWithHref(final String objectURI) {
+		return new ShortExtendendBasicDMPDTO(uuid, name, description, objectURI);
+	}
+
+	public static ShortExtendendBasicDMPDTO of(final ExtendedBasicDMPJPAObject pojo, final URI objectURI) {
+		final String uriString = objectURI == null ? null : objectURI.toString();
+		return new ShortExtendendBasicDMPDTO(
+				pojo.getUuid(), pojo.getName(), pojo.getDescription(), uriString);
+	}
+}
