858311a6d9cc65ba2c810e5e459be35ba5fb676b
Thomas Gängler <tgaengler@avantgarde-labs.de>
2015-08-27 10:17:19 +0200

    [WIP][DD-74] re-factored converter event recorder

--- controller/src/main/java/org/dswarm/controller/eventbus/XMLConverterEventRecorder.java
+++ controller/src/main/java/org/dswarm/controller/eventbus/XMLConverterEventRecorder.java
@@ -15,15 +15,8 @@
  */
 package org.dswarm.controller.eventbus;
 
-import java.util.Collection;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 
-import javax.ws.rs.core.Response;
-
-import com.fasterxml.jackson.databind.JsonNode;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
@@ -31,21 +24,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import rx.Observable;
 import rx.Scheduler;
-import rx.schedulers.Schedulers;
 
-import org.dswarm.controller.DMPControllerException;
 import org.dswarm.converter.flow.XMLSourceResourceGDMStmtsFlow;
 import org.dswarm.converter.flow.XmlResourceFlowFactory;
-import org.dswarm.graph.json.Model;
-import org.dswarm.graph.json.Resource;
-import org.dswarm.persistence.DMPPersistenceError;
-import org.dswarm.persistence.DMPPersistenceException;
-import org.dswarm.persistence.model.resource.Configuration;
+import org.dswarm.persistence.model.internal.gdm.GDMModel;
 import org.dswarm.persistence.model.resource.DataModel;
-import org.dswarm.persistence.model.resource.UpdateFormat;
-import org.dswarm.persistence.model.resource.utils.ConfigurationStatics;
-import org.dswarm.persistence.model.resource.utils.ResourceStatics;
-import org.dswarm.persistence.monitoring.MonitoringHelper;
 import org.dswarm.persistence.monitoring.MonitoringLogger;
 import org.dswarm.persistence.service.InternalModelServiceFactory;
 import org.dswarm.persistence.service.internal.graph.util.SchemaDeterminator;
@@ -57,17 +40,16 @@ import org.dswarm.persistence.service.internal.graph.util.SchemaDeterminator;
  * @author tgaengler
  */
 @Singleton
-public class XMLConverterEventRecorder {
+public class XMLConverterEventRecorder extends ConverterEventRecorder {
 
 	private static final Logger LOG = LoggerFactory.getLogger(XMLConverterEventRecorder.class);
+
+	private static final String TYPE = "XML";
 
 	/**
 	 * The internal model service factory
 	 */
-	private final InternalModelServiceFactory      internalServiceFactory;
 	private final Provider<XmlResourceFlowFactory> xmlFlowFactory;
-	private final Provider<MonitoringLogger>       loggerProvider;
-	private final Provider<SchemaDeterminator>     schemaDeterminatorProvider;
 
 	/**
 	 * Creates a new event recorder for converting XML documents with the given internal model service factory and event bus.
@@ -80,193 +62,25 @@ public class XMLConverterEventRecorder {
 			final Provider<XmlResourceFlowFactory> xmlFlowFactory,
 			final Provider<MonitoringLogger> loggerProvider,
 			final Provider<SchemaDeterminator> schemaDeterminatorProvider) {
-		internalServiceFactory = internalModelServiceFactory;
+
+		super(internalModelServiceFactory, loggerProvider, schemaDeterminatorProvider, TYPE);
+
 		this.xmlFlowFactory = xmlFlowFactory;
-		this.loggerProvider = loggerProvider;
-		this.schemaDeterminatorProvider = schemaDeterminatorProvider;
 	}
 
-	/**
-	 * Processes the XML document of the data model of the given event and persists the converted data.
-	 *
-	 * @param event an converter event that provides a data model
-	 */
-	// @Subscribe
-	public void processDataModel(final XMLConverterEvent event) throws DMPControllerException {
+	@Override
+	protected Observable<GDMModel> convertData(final DataModel dataModel, final boolean utiliseExistingSchema, final Scheduler scheduler,
+			final String path, final boolean hasSchema) {
 
-		final DataModel dataModel = event.getDataModel();
-		final UpdateFormat updateFormat = event.getUpdateFormat();
-		final boolean enableVersioning = event.isEnableVersioning();
+		final CompletableFuture<XMLSourceResourceGDMStmtsFlow> futureFlow = CompletableFuture
+				.supplyAsync(() -> xmlFlowFactory.get().fromDataModel(dataModel, utiliseExistingSchema));
+		final Observable<XMLSourceResourceGDMStmtsFlow> obserableFlow = Observable.from(futureFlow);
 
-		try (final MonitoringHelper ignore = loggerProvider.get().startIngest(dataModel)) {
+		return obserableFlow.subscribeOn(scheduler).flatMap(flow -> {
 
-			processDataModel(dataModel, updateFormat, enableVersioning);
-		}
-	}
+			LOG.debug("process xml data resource at '{}' into data model '{}' (has schema = '{}')", path, dataModel.getUuid(), hasSchema);
 
-	public void processDataModel(final DataModel dataModel, final UpdateFormat updateFormat, final boolean enableVersioning)
-			throws DMPControllerException {
-
-		final Observable<org.dswarm.persistence.model.internal.Model> result = doIngest(dataModel, false, Schedulers.newThread());
-
-		try {
-
-			final Observable<Response> writeResponse = internalServiceFactory.getInternalGDMGraphService()
-					.updateObject(dataModel.getUuid(), result, updateFormat, enableVersioning);
-
-			//LOG.debug("before to blocking");
-
-			// TODO: delegate observable
-			writeResponse.toBlocking().firstOrDefault(null);
-
-			LOG.debug("processed xml data resource into data model '{}'", dataModel.getUuid());
-		} catch (final DMPPersistenceException e) {
-
-			final String message = String.format("couldn't persist the converted data of data model '%s'", dataModel.getUuid());
-
-			XMLConverterEventRecorder.LOG.error(message, e);
-
-			throw new DMPControllerException(String.format("%s %s", message, e.getMessage()), e);
-		}
-	}
-
-	public Observable<org.dswarm.persistence.model.internal.Model> doIngest(final DataModel dataModel, final boolean utiliseExistingSchema,
-			final Scheduler scheduler)
-			throws DMPControllerException {
-
-		// TODO: enable monitoring here
-
-		LOG.debug("try to process xml data resource into data model '{}' (utilise existing schema = '{}')", dataModel.getUuid(),
-				utiliseExistingSchema);
-
-		try {
-
-			final SchemaDeterminator schemaDeterminator = schemaDeterminatorProvider.get();
-			final DataModel freshDataModel = schemaDeterminator.getSchemaInternal(dataModel.getUuid());
-			final boolean isSchemaAnInbuiltSchema = schemaDeterminator.isSchemaAnInbuiltSchema(freshDataModel);
-			final boolean hasSchema = isSchemaAnInbuiltSchema || utiliseExistingSchema;
-
-			final AtomicInteger counter = new AtomicInteger(0);
-			final AtomicLong statementCounter = new AtomicLong(0);
-
-			//LOG.debug("XML records = '{}'", gdmModels.size());
-
-			final String path = dataModel.getDataResource().getAttribute(ResourceStatics.PATH).asText();
-
-			final CompletableFuture<XMLSourceResourceGDMStmtsFlow> futureFlow = CompletableFuture
-					.supplyAsync(() -> xmlFlowFactory.get().fromDataModel(dataModel, utiliseExistingSchema));
-			final Observable<XMLSourceResourceGDMStmtsFlow> obserableFlow = Observable.from(futureFlow);
-
-			return obserableFlow.subscribeOn(scheduler).flatMap(flow -> {
-
-				LOG.debug("process xml data resource at '{}' into data model '{}' (has schema = '{}')", path, dataModel.getUuid(), hasSchema);
-
-				return flow.applyResource(path);
-			}).filter(gdmModel -> {
-
-				try {
-
-					final Model model = gdmModel.getModel();
-
-					if (model == null) {
-
-						LOG.debug("model is not available");
-
-						return false;
-					}
-
-					final Collection<Resource> resources = model.getResources();
-
-					if (resources == null || resources.isEmpty()) {
-
-						LOG.debug("resources from model are not available");
-
-						return false;
-					}
-
-					statementCounter.addAndGet(model.size());
-
-					if (counter.incrementAndGet() == 1) {
-
-						LOG.debug(
-								"transformed first record of xml data resource at to GDM for data model '{}' with '{}' statements (data resource at '{}')",
-								dataModel.getUuid(), statementCounter.get(), path);
-					}
-
-					schemaDeterminator.optionallyEnhancedDataModel(freshDataModel, gdmModel, model, hasSchema);
-
-					//final int current = counter.incrementAndGet();
-
-					//LOG.debug("XML resource number '{}' with '{}' and resources size = '{}'", current, resources.iterator().next().getUri(), resources.size());
-
-					return true;
-				} catch (DMPPersistenceException e) {
-
-					final String message = String
-							.format("something went wrong, while data model enhancement for data model '%s'", freshDataModel.getUuid());
-
-					LOG.error(message, e);
-
-					throw DMPPersistenceError.wrap(e);
-				}
-			}).cast(org.dswarm.persistence.model.internal.Model.class).doOnCompleted(
-					() -> {
-
-						final int recordCount = counter.get();
-
-						if (recordCount == 0) {
-
-							final Configuration configuration = dataModel.getConfiguration();
-							final Optional<JsonNode> optionalRecordTagNode = Optional.ofNullable(
-									configuration.getParameter(ConfigurationStatics.RECORD_TAG));
-
-							final Optional<String> optionalRecordTag;
-
-							if (optionalRecordTagNode.isPresent()) {
-
-								optionalRecordTag = Optional.ofNullable(optionalRecordTagNode.get().asText(null));
-							} else {
-
-								optionalRecordTag = Optional.empty();
-							}
-
-							final String messageStart = String.format(
-									"couldn't transform any record from xml data resource at '%s' to GDM for data model '%s'; ", path,
-									dataModel.getUuid());
-
-							final StringBuilder messageSB = new StringBuilder();
-							messageSB.append(messageStart);
-
-							if (optionalRecordTag.isPresent()) {
-
-								messageSB.append("maybe you set a wrong record tag (current one = '").append(optionalRecordTag.get()).append("')");
-							} else {
-
-								messageSB.append("maybe because you set no record tag");
-							}
-
-							throw new RuntimeException(messageSB.toString());
-						}
-
-						LOG.debug(
-								"transformed xml data resource at to GDM for data model '{}' - transformed '{}' records with '{}' statements (data resource at '{}')",
-								dataModel.getUuid(), recordCount, statementCounter.get(), path);
-					}).doOnSubscribe(
-					() -> LOG.debug("subscribed to XML ingest"));
-		} catch (final NullPointerException e) {
-
-			final String message = String.format("couldn't convert the XML data of data model '%s'", dataModel.getUuid());
-
-			XMLConverterEventRecorder.LOG.error(message, e);
-
-			throw new DMPControllerException(String.format("%s %s", message, e.getMessage()), e);
-		} catch (final Exception e) {
-
-			final String message = String.format("really couldn't convert the XML data of data model '%s'", dataModel.getUuid());
-
-			XMLConverterEventRecorder.LOG.error(message, e);
-
-			throw new DMPControllerException(String.format("%s %s", message, e.getMessage()), e);
-		}
+			return flow.applyResource(path);
+		});
 	}
 }
